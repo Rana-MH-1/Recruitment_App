d61d02f0e1e12662b6d8279de1279a635760d07a
syrineMabrouk <sirine.mabrouk2016@gmail.com>
2021-09-13 11:27:21 +0200

    essai home intro védio

--- client_side/src/Pages/Home.js
+++ client_side/src/Pages/Home.js
@@ -9,6 +9,8 @@ const home = () => {
         <section className="hero">
           <div className="background-image" />
           <div className="hero-content-area">
+         
+          
             <h1>Life is a party!</h1>
             <h3>Unmissable Adventure Tours With Your Friends</h3>
             <a href="#" className="btn">Contact Us Now</a>
