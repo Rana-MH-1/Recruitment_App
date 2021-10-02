cfd2a3f5e873326f5ddabb24553a343534f9d2b5
Rana-MH-1 <rana.meftah@enis.tn>
2021-10-03 00:22:51 +0100

    change port

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
 
 app.use(express.json({ limit: '50mb' }))
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.use(cors())
 app.use("/Uploads",express.static(__dirname+"/Uploads"))
