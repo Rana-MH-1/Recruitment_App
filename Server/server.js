686c0875d27ebe6e39bc70823bbf336fffb8c5ef
Rana-MH-1 <rana.meftah@enis.tn>
2021-10-03 16:27:18 +0100

    change to proxy

--- Server/server.js
+++ Server/server.js
@@ -29,7 +29,7 @@ const mongoose = require('mongoose');
 // };
 // app.use(allowCrossDomain);
 app.use(express.json({ limit: '50mb' }))
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use("/Uploads",express.static(__dirname+"/Uploads"))
