ac871829c7e2471b664feaa961af4975b396c1ed
Rana-MH-1 <rana.meftah@enis.tn>
2021-10-03 01:51:11 +0100

    add orgin in cors

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 app.use(express.json({ limit: '50mb' }))
 const PORT = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors({ origin: "http://localhost:8080", credentials: true }))
 app.use("/Uploads",express.static(__dirname+"/Uploads"))
 
 //routes
