cd031f5800d93d6bc900216c5324573e39898d54
Rana-MH-1 <rana.meftah@enis.tn>
2021-10-05 00:39:55 +0100

    set up upload.

--- Server/server.js
+++ Server/server.js
@@ -32,7 +32,7 @@ app.use(express.json({ limit: '50mb' }))
 const PORT = process.env.PORT || 5000;
 
 app.use(cors())
-app.use("/Uploads",express.static(__dirname+"/Uploads"))
+app.use(express.static(__dirname+"/Uploads"))
 
 //routes
 app.use('/api/Posts', require('./Routes/PostRecuiterRouter'))
